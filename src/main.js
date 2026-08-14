168db39af57d98020043f339c8ea27c34c9f4845
muriloHx <twitterfo131@gmail.com>
2026-08-14 16:03:59 -0300

    Adicao de mais regras

--- src/main.js
+++ src/main.js
@@ -2,19 +2,19 @@ import { loadRuleFile } from "./services/ruleLoader.js";
 import { evaluateChecklist } from "./services/evaluationService.js";
 
 async function main() {
-  const checklist = await loadRuleFile("src/rules/nbr9050-2020/portas.yaml");
 
+
+  const checklist_sanitario = await loadRuleFile("rules/nbr9050-2020/sanitario_acessivel.yaml")
   const dados = {
-    largura_vao: 0.85,
-    altura_vao: 2.10,
-    tipo_macaneta: "alavanca",
-    comprimento_macaneta: 100,
-    afastamento_macaneta: 40,
-    altura_macaneta: 0.95,
-    desnivel_soleira: 4
-  };
-
-  const resultados = evaluateChecklist(checklist, dados);
+    diametro_giro: 1.3,
+    vao_livre_porta_sanitario: 2,
+    comprimento_puxador_sanitario: 1,
+    altura_assento_bacia: 2.1,
+    eixo_bacia_parede: 1.2,
+    altura_lavatorio_borda: 2,
+    altura_livre_sob_lavatorio: 2.5,
+  }
+  const resultados = evaluateChecklist(checklist_sanitario, dados);
 
   console.table(resultados);
 }
