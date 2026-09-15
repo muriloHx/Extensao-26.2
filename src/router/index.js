d51ba1cc28a62f61751fab40c328d0f1b39971d5
Murilo <twitterfo131@gmail.com>
2026-09-15 17:49:37 -0300

    Atualizar o index.js

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,6 @@ import EnvironmentPage from "../pages/EnvironmentPage.vue";
 import ElementPage from "../pages/ElementPage.vue";
 import EvaluationPage from "../pages/EvaluationPage.vue";
 import RulesPage from "../pages/RulesPage.vue";
-export default createRouter({ history: createWebHistory(), routes: [
+export default createRouter({ history: createWebHistory(import.meta.env.BASE_URL), routes: [
   { path: "/", redirect: "/projects" }, { path: "/projects", component: ProjectsPage }, { path: "/projects/:projectId", component: ProjectPage, props: true }, { path: "/projects/:projectId/environments/:environmentId", component: EnvironmentPage, props: true }, { path: "/elements/new", component: ElementPage }, { path: "/elements/:elementId", component: ElementPage, props: true }, { path: "/elements/:elementId/evaluation", component: EvaluationPage, props: true }, { path: "/rules", component: RulesPage }, { path: "/rules/documentation", component: RulesPage, props: { documentation: true } }, { path: "/rules/:source/:groupId", component: RulesPage, props: true }, { path: "/:pathMatch(.*)*", redirect: "/projects" }
 ] });
